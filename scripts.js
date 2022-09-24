cbcf59190e3aea14f94428d2f8a4ef6d8f58dd7a
Taylor Brown <4754387+skylon07@users.noreply.github.com>
2022-09-23 19:29:10 -0600

    Add bootstrap and navbar (+scripts)

--- scripts.js
+++ scripts.js
@@ -12,3 +12,20 @@ async function alertOnFail(scopeName, callback, alertFn=alert) {
         alertFn(`In "${scopeName}" -- ${e}`)
     }
 }
+
+function setNavbarName(name) {
+    const navbar = _getNavbar()
+    const navbarBrand = navbar.querySelector(".navbar-brand")
+    navbarBrand.innerHTML = `Arcade -- ${name}`
+}
+
+function setNavbarActive(idx) {
+    const navbar = _getNavbar()
+    const navbarItems = navbar.querySelector(".navbar-nav")
+    const navbarItem = navbarItems.children[idx]
+    navbarItem.querySelector("a").classList.add("active")
+}
+
+function _getNavbar() {
+    return document.getElementById('navbar')
+}
