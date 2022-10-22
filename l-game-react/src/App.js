935ad5e1bfcbb7e3c3a56013aa546ef67f3281f8
Taylor Brown <4754387+skylon07@users.noreply.github.com>
2022-10-22 13:10:13 -0600

    Create L-game placeholder
    - Created react app via CRA
    - Updated README
    - Ran build and deployed according to README
    - Added an iframe to load built app

---
+++ l-game-react/src/App.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return <div className="App">
+        <h1>This is where the game would go...</h1>
+    </div>
+}
+
+export default App;
