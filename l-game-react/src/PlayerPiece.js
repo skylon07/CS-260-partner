aeba69e1d67f19c17fe57b485c2bb747bd06b463
Taylor Brown <4754387+skylon07@users.noreply.github.com>
2022-10-23 23:32:39 -0600

    Update PlayerPiece size on window resize

--- l-game-react/src/PlayerPiece.js
+++ l-game-react/src/PlayerPiece.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react"
+import { useRef } from "react"
 import { PlayerPosition } from "./gamestate"
 
 import './PlayerPiece.css'
@@ -10,31 +10,21 @@ export default function PlayerPiece({position}) {
 
     const pieceRef = useRef()
 
-    const [squareSize, setSquareSize] = useState(null)
-    useEffect(() => {
-        const pieceElem = pieceRef.current
-        setSquareSize(pieceElem.offsetWidth / 4)
-    }, [])
-
     const pieceSquares = position.toPositionPath().map((pathPosition) => {
-        if (squareSize === null) {
-            return null
-        }
-
-        const offsetY = pathPosition.rowIdx * squareSize
-        const offsetX = pathPosition.colIdx * squareSize
+        const offsetYIdx = pathPosition.rowIdx
+        const offsetXIdx = pathPosition.colIdx
 
         // DEBUG: experimental style for demonstration purposes
         const style = {
             position: "absolute",
-            top: `${offsetY}px`,
-            left: `${offsetX}px`,
-            width: `${squareSize}px`,
-            height: `${squareSize}px`,
+            top: `calc(${offsetYIdx} * 25%)`,
+            left: `calc(${offsetXIdx} * 25%)`,
+            width: `25%`,
+            height: `25%`,
             backgroundColor: "green",
         }
         // TODO: add a PlayerPiece-Square CSS class
-        return <div key={[offsetX, offsetY]} style={style}></div>
+        return <div key={[offsetXIdx, offsetYIdx]} style={style}></div>
     })
 
     return <div ref={pieceRef} className="PlayerPiece">
